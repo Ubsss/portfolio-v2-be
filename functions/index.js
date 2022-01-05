6c007ac510c40f6e633b2fdc4382c73b40e20bb2
Uchechukwu Uboh <uboh@MacBooky-Pro.local>
2022-01-04 18:58:56 -0600

    completed initial structure of be

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,67 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const twilioClient = require("twilio");
+const cors = require("cors")({ origin: true });
+const uboh = require("./modules/Uboh");
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+const smsClient = twilioClient(
+  functions.config().sms.account_sid,
+  functions.config().sms.auth_token
+);
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+exports.uboh = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    try {
+      if (req.method !== "POST" || !req.body)
+        throw { code: 400, message: "Invalid sms request" };
+
+      if (
+        !req.headers.authentication ||
+        !req.headers.authentication.startsWith("Bearer ")
+      ) {
+        throw { code: 400, message: "Unauthorized access" };
+      }
+
+      let token = req.headers.authentication.split("Bearer ")[1];
+
+      let action = req.body.action;
+
+      auth
+        .verifyIdToken(token)
+        .then(() => {
+          switch (action) {
+            case "addMessage":
+              uboh.addMessage(req.body.message, res, db);
+              break;
+            case "addLog":
+              uboh.addLog(req.body.log, res, db);
+              break;
+            case "sendSMS":
+              uboh.sendSMS(res, smsClient, req.body.smsContent);
+              break;
+            case "addAdvice":
+              uboh.addAdvice(res, req.body.advice);
+              break;
+            default:
+              res.json({ code: 400, message: "Invalid sms request" });
+              break;
+          }
+          return;
+        })
+        .catch((err) => {
+          console.error(err);
+          return res.status(400).json({
+            code: 400,
+            message: "Unauthorized access",
+          });
+        });
+    } catch (error) {
+      console.error(error);
+      res.json({
+        code: error.code && error.code === 400 ? error.code : 500,
+        message:
+          error.code && error.code === 400 ? error.message : "Internal error",
+      });
+    }
+  });
 });
--- functions/modules/Uboh.js
+++ functions/modules/Uboh.js
@@ -1,203 +1,109 @@
-class Uboh {
-  /**
-   * Store new user to firestore db, updates existing records if present
-   * @param  {map}                user user object
-   * @param  {express response}   res  express response object
-   * @param  {firebase firestore} db   firebase firestore object
-   */
-  async addUser(data, res, db) {
-    try {
-      if (!data.email) throw { code: 400, message: "Invalid user object" };
-      let gettingDoc = await db
-        .collection("uboh")
-        .doc("web")
-        .collection("users")
-        .doc(data.email)
-        .get();
-      if (gettingDoc.exists) {
-        let userData = gettingDoc.data();
-        if (data.phone && userData.phone != data.phone) {
-          await db
-            .collection("uboh")
-            .doc("web")
-            .collection("users")
-            .doc(data.email)
-            .update({ phone: data.phone });
-        }
-      } else {
-        let date = new Date();
-        let newUser = Object.assign({ created: date.toUTCString() }, data);
-        await db
-          .collection("uboh")
-          .doc("web")
-          .collection("users")
-          .doc(data.email)
-          .set(newUser);
-      }
-      res.json({
-        code: 200,
-        message: "Add or updated user",
-      });
-    } catch (error) {
-      console.error(error);
-      res.json({
-        code: error.code && error.code === 400 ? error.code : 500,
-        message:
-          error.code && error.code === 400
-            ? error.message
-            : "Internal error, please try again later",
-      });
-    }
+/**
+ * Add connect message to db
+ * @param  {map}                message user message
+ * @param  {express response}   res     express response object
+ * @param  {firebase firestore} db      firebase firestore object
+ */
+exports.addMessage = async function (message, res, db) {
+  try {
+    if (!message.email || !message.type || !message.message)
+      throw { code: 400, message: "Invalid message object" };
+
+    let date = new Date();
+    let newMessage = Object.assign({ created: date.toUTCString() }, message);
+
+    await db
+      .collection("messages")
+      .doc(`${message.email}_${date.toUTCString().replace(/\s+/g, "")}`)
+      .set(newMessage);
+
+    res.json({
+      code: 200,
+      message: "Message added",
+    });
+  } catch (error) {
+    console.error(error);
+    res.json({
+      code: error.code && error.code === 400 ? error.code : 500,
+      message:
+        error.code && error.code === 400
+          ? error.message
+          : "Internal error, pl-ease try again later",
+    });
   }
+};
 
-  /**
-   * Add connect message to db
-   * @param  {map}                message user message
-   * @param  {express response}   res     express response object
-   * @param  {firebase firestore} db      firebase firestore object
-   */
-  async addMessage(message, res, db) {
-    try {
-      if (!message.email || !message.type || !message.message)
-        throw { code: 400, message: "Invalid message object" };
+/**
+ * Add log to db
+ * @param  {map}                log log object
+ * @param  {express response}   res express response object
+ * @param  {firebase firestore} db  firebase firestore object
+ */
+exports.addLog = async function (log, res, db) {
+  try {
+    if (!log.message) throw { code: 400, message: "Invalid log object" };
 
-      let date = new Date();
-      let newMessage = Object.assign({ created: date.toUTCString() }, message);
+    let date = new Date();
+    let newLog = Object.assign({ created: date.toUTCString() }, log);
 
-      await db
-        .collection("uboh")
-        .doc("web")
-        .collection("messages")
-        .doc(`${message.email}_${date.toUTCString().replace(/\s+/g, "")}`)
-        .set(newMessage);
+    await db.collection("logs").doc(date.toUTCString()).set(newLog);
 
-      res.json({
-        code: 200,
-        message: "Message added",
-      });
-    } catch (error) {
-      console.error(error);
-      res.json({
-        code: error.code && error.code === 400 ? error.code : 500,
-        message:
-          error.code && error.code === 400
-            ? error.message
-            : "Internal error, please try again later",
-      });
-    }
+    res.json({
+      code: 200,
+      message: "Log added",
+    });
+  } catch (error) {
+    console.error(error);
+    res.json({
+      code: error.code && error.code === 400 ? error.code : 500,
+      message:
+        error.code && error.code === 400
+          ? error.message
+          : "Internal error, please try again later",
+    });
   }
+};
 
-  /**
-   * Add score to db
-   * @param  {map}                score score object
-   * @param  {express response}   res   express response object
-   * @param  {firebase firestore} db    firebase firestore object
-   */
-  async addScore(score, res, db) {
-    try {
-      if (!score.name || !score.score)
-        throw { code: 400, message: "Invalid score object" };
-
-      let date = new Date();
-      let newScore = Object.assign({ created: date.toUTCString() }, score);
-
-      await db
-        .collection("uboh")
-        .doc("web")
-        .collection("topScores")
-        .doc(`${score.email}_${date.toUTCString()}_${score.score}`)
-        .set(newScore);
-
-      res.json({
-        code: 200,
-        message: "Score added",
-      });
-    } catch (error) {
-      console.error(error);
-      res.json({
-        code: error.code && error.code === 400 ? error.code : 500,
-        message:
-          error.code && error.code === 400
-            ? error.message
-            : "Internal error, please try again later",
-      });
-    }
+/**
+ * Sends an sms to a provided number with message
+ * @param {Express response}  res        express response object
+ * @param {Twilio SMS Object} sms        twilio sms object
+ * @param {string}            message    message to be sent
+ */
+exports.sendSMS = async function (res, sms, smsContent) {
+  try {
+    if (!res || !sms || !message) throw new Error("Invalid sms request");
+    let sendMessage = await sms.messages.create({
+      to: functions.config().sms.personal_number,
+      from: functions.config().sms.twilio_number,
+      body: message,
+    });
+    console.log(sendMessage);
+    res.json({ status: "Success", message: "Sent sms message" });
+  } catch (error) {
+    res.json({
+      status: "Error",
+      message: error.message || "Unable to send sms",
+    });
   }
+};
 
-  //   /**
-  //    * Update top scores
-  //    * @param  {map}                score score object
-  //    * @param  {firebase firestore} db    firebase firestore object
-  //    */
-  //   async updateHighScores(score, db) {
-  //     try {
-  //       let date = new Date();
-  //       let newScore = Object.assign({ created: date.toUTCString() }, score);
+/**
+ * add an advice to db
+ * @param {express response} res        express response object
+ * @param {array[advice]}    adviceList list of advice
+ */
+exports.addAdvice = async function (res, adviceList) {
+  console.log("adding advice to db");
+};
 
-  //       await db
-  //         .collection("uboh")
-  //         .doc("web")
-  //         .collection("scores")
-  //         .doc(`${score.email}_${date.toUTCString()}_${score.score}`)
-  //         .set(newScore);
-
-  //       res.json({
-  //         code: 200,
-  //         message: "Score added",
-  //       });
-  //     } catch (error) {
-  //       console.error(error);
-  //       res.json({
-  //         code: error.code && error.code === 400 ? error.code : 500,
-  //         message:
-  //           error.code && error.code === 400
-  //             ? error.message
-  //             : "Internal error, please try again later",
-  //       });
-  //     }
-  //   }
-
-  /**
-   * Add log to db
-   * @param  {map}                log log object
-   * @param  {express response}   res express response object
-   * @param  {firebase firestore} db  firebase firestore object
-   */
-  async addLog(log, res, db) {
-    try {
-      if (!log.message) throw { code: 400, message: "Invalid log object" };
-
-      let date = new Date();
-      let newLog = Object.assign({ created: date.toUTCString() }, log);
-
-      await db
-        .collection("uboh")
-        .doc("web")
-        .collection("logs")
-        .doc(date.toUTCString())
-        .set(newLog);
-
-      res.json({
-        code: 200,
-        message: "Log added",
-      });
-    } catch (error) {
-      console.error(error);
-      res.json({
-        code: error.code && error.code === 400 ? error.code : 500,
-        message:
-          error.code && error.code === 400
-            ? error.message
-            : "Internal error, please try again later",
-      });
-    }
-  }
-
-  /**
-   * Implement
-   * Get advice function
-   * update likes function
-   */
+/**
+ * validate the advice object
+ * @param {number} likes     total likes for advice
+ * @param {string} advice    the advice itself
+ * @param {string} author    advice owner
+ * @param {string} category  type of advice
+ */
+function validateAdvice(likes, advice, author, category) {
+  console.log("validating advice");
 }
-
-module.exports = new Uboh();
